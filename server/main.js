a4f3377df002a6d78d588b35034af4da2b6bcf62
Ihor Haiduk <cookkycok@gmail.com>
2016-10-25 13:03:28 +0300

    mrt test

--- server/main.js
+++ server/main.js
@@ -5,3 +5,8 @@ import '../import/backend/tasks.js';
 /*Meteor.startup(() => {
   // code to run on server at startup
 });*/
+
+// Meteor.startup(function() {
+//     Meteor.Cluster.init(); //assumes you are running redis-server locally
+//     //Meteor.Cluster.sync(Posts, Notifications, Comments); //these are my collections
+// });
