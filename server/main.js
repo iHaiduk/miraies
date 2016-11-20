4cff9538953608837051ee7588e7d0a99f64e8ee
Ihor Haiduk <cookkycok@gmail.com>
2016-11-20 21:29:52 +0200

    add graphql

--- server/main.js
+++ server/main.js
@@ -3,4 +3,13 @@ import '../imports/backend/task.js';
 
 Meteor.startup(() => {
   // code to run on server at startup
+
+    // const schema = new GraphQLSchema({
+    //     query,
+    //     mutation
+    // });
+    //
+    // GraphQL.registerSchema('Blog', schema);
+
+
 });
